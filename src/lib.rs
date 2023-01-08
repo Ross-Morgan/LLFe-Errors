a3a8ca04a2e6d38636c90a083b7c89975d5f2987
Ross Morgan <morgan.ross.512@gmail.com>
2023-01-08 01:03:08 +0000

    add custom result type

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod position;
 pub use error::LLFeError;
 pub use position::Position;
 
+pub type Result<T> = std::result::Result<T, LLFeError>;
+
 pub fn new_error<T: ToString>(description: T, caused_by: Option<Box<LLFeError>>) -> LLFeError {
     LLFeError {
         description: description.to_string(),
